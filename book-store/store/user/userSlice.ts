f19e382df834988a5089a18056b6c58dcc8ed8db
aruadecarvalho <aruadecarvalho@gmail.com>
2023-01-17 18:53:13 -0300

    feature: redux store and user reducer configuration

---
+++ book-store/store/user/userSlice.ts
@@ -0,0 +1,16 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  currentUser: null,
+  isLoading: false,
+  error: null,
+};
+
+const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {}
+});
+
+// export const {} = userSlice.actions;
+export default userSlice.reducer;
