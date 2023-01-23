14d0c28e700bf9ea9bfdc5ceeb2390fa88c6476b
NayoBaez <naomi.singh@hotmail.com>
2023-01-23 19:12:25 -0400

    Feature: Created Card Component

--- book-store/src/Components/Body/Body.js
+++ book-store/src/Components/Body/Body.js
@@ -1,15 +1,22 @@
 import React from "react";
+import Card from "../Card/Card.js";
+import data from "../data/data.js";
 import "./Body.css";
 
 export default function Body() {
+  const cards = data.map((card) => {
+    return <Card key={card.id} card={card} />;
+  });
+
   return (
-    <div>
+    <div className="body">
       <div className="banner">
         LIMITED TIME OFFER - 50% OFF ALL BOOKS WITH SOFT COVERS{" "}
       </div>
       <div>
-        <img src="../images/main-image.png" alt="books" className="cover-img" />
+        <img src="../images/main-image.png" alt="books" className="cover" />
       </div>
+      <section className="cards-list">{cards}</section>
     </div>
   );
 }
---
+++ book-store/src/Components/data/data.js
@@ -0,0 +1,46 @@
+const data = [
+  {
+    id: 1,
+    title: "RRETOS HHUMANOS",
+    author: "Antontio Walker",
+    description:
+      "Tiempos de pandemia. Una historia amena que genera poderosas reflexiones ....",
+    price: 49.99,
+    coverImg: "rretos-hhumanos.png",
+    stats: {
+      rating: 5.0,
+      reviewCount: 13,
+    },
+    available: 0,
+  },
+  {
+    id: 2,
+    title: "PASIÖN",
+    author: "Antontio Walker",
+    description:
+      "¿Por qué hace falta pasión en las organizaciones? Estamos viviendo un cambio",
+    price: 49.99,
+    coverImg: "pasion.png",
+    stats: {
+      rating: 5.0,
+      reviewCount: 9,
+    },
+    available: 15,
+  },
+  {
+    id: 3,
+    title: "EL MUNDO DE LAS EMOCIONES",
+    author: "Antontio Walker",
+    description:
+      "La primera guía fácil para desarrollar por uno mismo la inteligencia emocional ...",
+    price: 49.99,
+    coverImg: "elmundo-delas-emociones.png",
+    stats: {
+      rating: 5.0,
+      reviewCount: 16,
+    },
+    available: 5,
+  },
+];
+
+export default data;
