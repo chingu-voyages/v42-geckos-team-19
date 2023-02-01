f393e054edff600bb14c857280deab0e3d99084a
NayoBaez <naomi.singh@hotmail.com>
2023-02-01 16:56:43 -0400

    Feature: Added Styling to Product Page

--- book-store/src/components/BookPage/BookPage.js
+++ book-store/src/components/BookPage/BookPage.js
@@ -2,6 +2,7 @@ import React from "react";
 import data from "../data/data";
 import {
   Stack,
+  HStack,
   Container,
   Center,
   Box,
@@ -17,6 +18,7 @@ import {
   TabPanels,
   Tab,
   TabPanel,
+  chakra,
 } from "@chakra-ui/react";
 
 export default function BookPage() {
@@ -56,7 +58,82 @@ export default function BookPage() {
           w={{ base: "100%", md: "50%" }}
           mb={{ base: "25px", md: "0" }}
         >
-          2
+          <Heading fontSize="40px" fontWeight="600" fontFamily="Poppins">
+            RRETOS HHUMANOS
+          </Heading>
+          <Text>By Antontio Walker</Text>
+          <HStack
+            spacing={{ base: 0, sm: 2 }}
+            flexWrap="wrap"
+            alignItems={{ base: "center", md: "left" }}
+            justifyContent={{ base: "center", md: "left" }}
+            py="10px"
+          >
+            <Image src="../images/review-rating.png" boxSize="20%"></Image>
+            <Text>5.0 (27) </Text>
+          </HStack>
+          <Divider orientation="horizontal" borderColor="#D9D9D9" my="20px" />
+
+          <Text fontSize="25px">$49.99</Text>
+          <HStack
+            spacing={{ base: 0, sm: 2 }}
+            flexWrap="wrap"
+            alignItems={{ base: "center", md: "left" }}
+            justifyContent={{ base: "center", md: "left" }}
+          >
+            <Button
+              h={12}
+              px={8}
+              color="white"
+              variant="outline"
+              borderColor="#E4573D"
+              fontSize="lg"
+              rounded="sm"
+              mb={{ base: 2, sm: 0 }}
+              lineHeight="20px"
+              bg="#E4573D"
+              _hover={{
+                bg: "white",
+                color: "#E4573D",
+              }}
+              fontFamily="Poppins"
+              fontWeight="300"
+              letterSpacing="2px"
+            >
+              Add to Cart
+            </Button>
+          </HStack>
+          <HStack
+            spacing={{ base: 0, sm: 2 }}
+            flexWrap="wrap"
+            alignItems={{ base: "center", md: "left" }}
+            justifyContent={{ base: "center", md: "left" }}
+          >
+            <Image src="../images/delivery-truck.png" h="70px"></Image>
+            <Text>
+              <chakra.span as="b">Approximate delivery</chakra.span> on
+              Thursday, January 19th
+            </Text>
+          </HStack>
+          <Stack>
+            <Box
+              borderRadius="md"
+              borderWidth="1px"
+              borderColor="#D9D9D9"
+              p="10px"
+              mt="20px"
+            >
+              <HStack
+                spacing="20"
+                flexWrap="wrap"
+                alignItems="center"
+                justifyContent="center"
+              >
+                <Text mr="10">Share with friends</Text>
+                <Text>Save to wishlist</Text>
+              </HStack>
+            </Box>
+          </Stack>
         </Box>
       </Stack>
 
@@ -88,19 +165,17 @@ export default function BookPage() {
                   doce historias de personajes muy diferentes, basadas en
                   situaciones casi reales, que hacen que cualquiera de ellos
                   pueda ser tu compañero, tu vecino… o tú mismo.
-                </Text>
-                <br />
-                <Text textAlign="left">
+                  <br />
+                  <br />
                   A través de las experiencias de nuestros protagonistas
                   caminamos por escenarios a veces desoladores compartiendo sus
                   sentimientos más sinceros para llegar a comprender cómo el
                   camino personal y el profesional al final suelen unirse en uno
                   solo. En este libro, el tercero de esta saga temática,
                   volvemos a constatar que la esencia de las empresas la forman
-                  las personas y sus sentimientos y emociones.{" "}
-                </Text>
-                <br />
-                <Text textAlign="left">
+                  las personas y sus sentimientos y emociones.
+                  <br />
+                  <br />
                   Tanto si has leído las dos entregas anteriores como si es la
                   primera vez que te adentras en el universo de Green
                   Technology, este texto único creado por once directores de
@@ -125,11 +200,26 @@ export default function BookPage() {
                   Ficha Técnica
                 </Heading>
                 <Text>
-                  COLECCIÓN GESTIÓN DE PERSONAS Y DEL TALENTO PVP: $49.99
-                  Rústica con solapas 14 x 22 cm 290 páginas ISBN:
-                  978-84-18811-24-1 Código IBIC: KJMB, KJMV2 BISAC: BUS030000
+                  COLECCIÓN GESTIÓN DE PERSONAS Y DEL TALENTO
+                  <br />
+                  <br />
+                  PVP: $49.99
+                  <br />
+                  Rústica con solapas <br />
+                  14 x 22 cm
+                  <br />
+                  290 páginas
+                  <br />
+                  <br /> ISBN: 978-84-18811-24-1
+                  <br />
+                  Código IBIC: KJMB, KJMV2
+                  <br />
+                  BISAC: BUS030000 <br />
                   BUS030000 BUSINESS & ECONOMICS / Human Resources & Personnel
-                  Management Peso: 380 gr
+                  Management
+                  <br />
+                  <br />
+                  Peso: 380 gr
                 </Text>
               </Box>
             </Stack>
