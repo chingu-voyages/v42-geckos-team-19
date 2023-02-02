194fc1157b2e5b41194e626b6c573bb59c945154
vivchc <viannc6284@gmail.com>
2023-02-02 14:37:41 -0500

    Added Footer to Body.js

---
+++ book-store/src/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Body from './Components/Body/Body.js';
+
+const App = () => {
+    return <Body />;
+};
+
+export default App;
---
+++ book-store/src/index.js
@@ -0,0 +1,29 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { Provider } from "react-redux";
+import { store } from "./store/rootReducer";
+import { ChakraProvider } from "@chakra-ui/react";
+import { extendTheme } from "@chakra-ui/react";
+import "@fontsource/poppins";
+import "@fontsource/libre-baskerville/400-italic.css";
+import "@fontsource/libre-baskerville";
+
+const theme = extendTheme({
+  fonts: {
+    fontFamily1: `'Libre Baskerville', sans-serif`,
+    fontFamily2: `'Poppins', sans-serif`,
+  },
+});
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <>
+    <Provider store={store}>
+      <ChakraProvider theme={theme}>
+        <App />
+      </ChakraProvider>
+    </Provider>
+  </>
+);
