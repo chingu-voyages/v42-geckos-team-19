88f36e32e80bc61a0560b04f0b7cd53302244d4d
NayoBaez <naomi.singh@hotmail.com>
2023-02-04 17:53:40 -0400

    Fix: component folder upper to lowercase Retry

---
+++ book-store/src/components/Body/Body.js
@@ -0,0 +1,28 @@
+import React from "react";
+import Faq from "../Faq/Faq";
+import data from "../data/data";
+import Hero from "../Hero/Hero";
+import BookCard from "../BookCard/BookCard";
+import HomeGrid from "../HomeGrid/HomeGrid";
+import { Stack, Container } from "@chakra-ui/react";
+import "./Body.css";
+
+export default function Body() {
+  const cards = data.map((card) => {
+    return <BookCard key={card.id} card={card} />;
+  });
+
+  return (
+    <Container maxW="1400px">
+      <Hero />
+      <Stack
+        direction={{ base: "column", md: "row" }}
+        spacing={{ base: 0, md: "50px" }}
+      >
+        {cards}
+      </Stack>
+      <HomeGrid />
+      <Faq />
+    </Container>
+  );
+}
---
+++ book-store/src/components/BookCard/BookCard.js
@@ -0,0 +1,91 @@
+import React from "react";
+import {
+  Card,
+  CardBody,
+  Heading,
+  Text,
+  Divider,
+  Image,
+  Stack,
+  Grid,
+  GridItem,
+  Button,
+  Box,
+} from "@chakra-ui/react";
+import "./BookCard.css";
+
+export default function BookCard(props) {
+  return (
+    <Box
+      display="flex"
+      alignItems="center"
+      justifyContent={{ base: "center", md: "space-around" }}
+    >
+      <Card
+        maxW={{ base: "lg", md: "md" }}
+        boxShadow="xl"
+        my={{ base: 10, md: 0 }}
+        mx={{ base: 0, md: 0 }}
+        fontFamily="Poppins"
+      >
+        <CardBody>
+          <Image
+            src={`../images/${props.card.coverImg}`}
+            alt={props.card.alt}
+            borderRadius="lg"
+            boxSize="100%"
+          />
+          <Stack mt="6" spacing="3">
+            <Heading size="sm">{props.card.title}</Heading>
+            <Text>{props.card.description}</Text>
+          </Stack>
+          <Divider my="6" borderColor="#D9D9D9" />
+          <Grid
+            templateRows="repeat(2, 1fr)"
+            templateColumns="repeat(2, 1fr)"
+            gap={2}
+            mb="5"
+          >
+            <GridItem w="100%" h="5">
+              <Text ml="3" color="#61625F">
+                Author
+              </Text>
+            </GridItem>
+            <GridItem w="100%" h="5">
+              <Text ml="20" color="#61625F">
+                Price
+              </Text>
+            </GridItem>
+            <GridItem w="100%" h="5">
+              <Text as="b" ml="3">
+                {props.card.author}
+              </Text>
+            </GridItem>
+            <GridItem w="100%" h="5">
+              <Text ml="20" as="b">
+                ${props.card.price}
+              </Text>
+            </GridItem>
+          </Grid>
+          <Stack align="center">
+            <Button
+              bgColor="white"
+              color="#E4573D"
+              colorScheme="E4573D"
+              size="md"
+              rounded="sm"
+              px="12"
+              py="6"
+              my="5"
+              variant="outline"
+              _hover={{ bg: "#E4573D", color: "white" }}
+              letterSpacing="2px"
+            >
+              Read More
+            </Button>
+          </Stack>
+        </CardBody>
+      </Card>
+    </Box>
+  );
+}
