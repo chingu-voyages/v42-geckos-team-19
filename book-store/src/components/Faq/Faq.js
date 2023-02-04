f01b82da2126dc08cc64b8e2353ae3cc7c6a2e1e
NayoBaez <naomi.singh@hotmail.com>
2023-02-04 18:22:27 -0400

    Fix:files stil giving errors

--- book-store/src/components/Faq/Faq.js
+++ book-store/src/components/Faq/Faq.js
@@ -76,7 +76,7 @@ export default function Faq() {
               <AccordionPanel pb={4} fontFamily="Poppins">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
                 eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                enim ad minim veniam, quis nostrud exercitation ullamcon laboris
                 nisi ut aliquip ex ea commodo consequat.
               </AccordionPanel>
             </AccordionItem>
