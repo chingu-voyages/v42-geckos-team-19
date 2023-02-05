ece96bd7e3616b9aaa160f6de416529c9bf9987a
aruadecarvalho <aruadecarvalho@gmail.com>
2023-02-04 21:48:51 -0300

    removed old navbar

--- book-store/src/App.js
+++ book-store/src/App.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import Body from './components/Body/Body.js';
+import React from "react";
 import { Route, Routes } from "react-router-dom";
-import Categories from './components/Categories/Categories.js';
+import Categories from "./components/Categories/Categories.js";
+import Home from "./pages/Home/Home";
 
 const App = () => {
-    return (
-        <Routes>
-          <Route>
-            <Route index element={<Categories/>} />
-            <Route path="categories/:param" element={<Categories />} />
-            </Route>
-        </Routes>
-    );
+  return (
+    <Routes>
+      <Route>
+        <Route index element={<Home />} />
+        <Route path="categories" element={<Categories />} />
+        <Route path="categories/:param" element={<Categories />} />
+      </Route>
+    </Routes>
+  );
 };
 
 export default App;
--- book-store/src/index.tsx
+++ book-store/src/index.tsx
@@ -7,14 +7,12 @@ import { store } from "./store/rootReducer";
 import { ChakraProvider } from "@chakra-ui/react";
 import { extendTheme } from "@chakra-ui/react";
 
-
 import "@fontsource/poppins";
 import "@fontsource/poppins/300.css";
 import "@fontsource/poppins/400.css";
 import "@fontsource/libre-baskerville";
 import "@fontsource/libre-baskerville/400-italic.css";
-import { BrowserRouter} from "react-router-dom";
-
+import { BrowserRouter } from "react-router-dom";
 
 const theme = extendTheme({
   fonts: {
