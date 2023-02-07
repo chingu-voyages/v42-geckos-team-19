723292156a0fd66d02c400fc6949de0652797caf
Stan <cyclinglinguist@gmail.com>
2023-02-07 17:06:16 -0600

    fix: still trying to resolve casing issue

--- book-store/src/hooks.ts
+++
@@ -1,6 +0,0 @@
-import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
-import type { RootState, AppDispatch } from "./store/rootReducer";
-
-type DispatchFunc = () => AppDispatch;
-export const useAppDispatch: DispatchFunc = useDispatch;
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
