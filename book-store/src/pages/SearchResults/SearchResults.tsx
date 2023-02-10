e9f74eb764c65c484ea75cb4b94f8060127fc57a
Stan <cyclinglinguist@gmail.com>
2023-02-10 11:22:29 -0600

    chore: change SearchResults to .tsx

--- book-store/src/pages/SearchResults/SearchResults.tsx
+++ book-store/src/pages/SearchResults/SearchResults.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { Box, Container } from "@chakra-ui/react";
 import "./SearchResults.css";
-import Footer from "../../components/Footer/Footer";
 
-export default function Home() {
+
+export default function SearchResults() {
   const cards = data.map((card) => {
     return <BookCard key={card.id} card={card} />;
   });
