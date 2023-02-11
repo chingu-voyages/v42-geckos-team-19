f3d17baaa7903a38b571399faf114c60f1c1cb85
NayoBaez <naomi.singh@hotmail.com>
2023-02-11 13:29:24 -0400

    Fix: remaining merge conflicts

--- book-store/src/pages/Home/Home.js
+++ book-store/src/pages/Home/Home.js
@@ -1,31 +1,17 @@
 import React from "react";
-<<<<<<< HEAD:book-store/src/components/Body/Body.js
-import Faq from "../Faq/Faq";
-import data from "../data/data";
-import Hero from "../Hero/Hero";
-import BookCard from "../BookCard/BookCard";
-import HomeGrid from "../HomeGrid/HomeGrid";
-import { Stack, Container, Box } from "@chakra-ui/react";
-import Checkout from "../../pages/Checkout/Checkout";
-import "./Body.css";
-import Footer from "../Footer/Footer";
-=======
 import Faq from "../../components/Faq/Faq";
 import data from "../../components/data/data";
 import Hero from "../../components/Hero/Hero";
 import BookCard from "../../components/BookCard/BookCard";
 import HomeGrid from "../../components/HomeGrid/HomeGrid";
-import Checkout from "../Checkout/Checkout";
-import { Box, Container } from "@chakra-ui/react";
+import { Container, Stack } from "@chakra-ui/react";
 import "./Home.css";
 import Footer from "../../components/Footer/Footer";
->>>>>>> development:book-store/src/pages/Home/Home.js
 
 export default function Home() {
   const cards = data.map((card) => {
     return <BookCard key={card.id} card={card} />;
   });
-
   return (
     <Container maxW="1400px">
       <Hero />
