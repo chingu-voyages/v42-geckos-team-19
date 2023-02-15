f6adb9e15b102cad51a305597a893b94cd8711ae
Stan <cyclinglinguist@gmail.com>
2023-02-15 10:37:27 -0600

    chore: remove superfluous Home.tsx

--- book-store/src/pages/SearchResults/SearchResults.tsx
+++ book-store/src/pages/SearchResults/SearchResults.tsx
@@ -2,12 +2,15 @@ import React from "react";
 import BookCard from "../../components/BookCard/BookCard";
 import { Box, Container } from "@chakra-ui/react";
 import "./SearchResults.css";
-
+import { useParams, useNavigate } from 'react-router-dom';
 
 export default function SearchResults() {
   // const cards = data.map((card) => {
   //   return <BookCard key={card.id} card={card} />;
   // });
+
+  const params = useParams();
+  console.log(params);
 
   return (
     <Container maxW="1400px">
