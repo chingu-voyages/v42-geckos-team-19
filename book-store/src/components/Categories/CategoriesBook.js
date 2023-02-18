b5b4232511a4cfe0dd5123af40620203142e0b25
NayoBaez <naomi.singh@hotmail.com>
2023-02-18 18:49:21 -0400

    Feature: updated styling in categories components

--- book-store/src/components/Categories/CategoriesBook.js
+++ book-store/src/components/Categories/CategoriesBook.js
@@ -1,190 +1,187 @@
-import React from 'react';
+import React from "react";
 import {
-    Box,
-    Card,
-    CardBody,
-    Heading,
-    Text,
-    Divider,
-    Image,
-    Stack,
-    Grid,
-    GridItem,
-    Button,
-    HStack,
-    Flex
-} from '@chakra-ui/react';
-import { useNavigate } from 'react-router-dom';
+  Box,
+  Card,
+  CardBody,
+  Heading,
+  Text,
+  Divider,
+  Image,
+  Stack,
+  Grid,
+  GridItem,
+  Button,
+  HStack,
+  Flex,
+} from "@chakra-ui/react";
+import { useNavigate } from "react-router-dom";
 
 export default function BookCard(props) {
-    const navigate = useNavigate();
+  console.log(props);
+  const navigate = useNavigate();
 
-    /* Truncate long titles */
-    function Title({ title }) {
-        if (title.length > 33) {
-            let str = '';
-            for (let i = 0; i < 33; i++) {
-                str += title[i];
-            }
-            str += '...';
-            return str;
-        } else {
-            return title;
-        }
+  /* Truncate long titles */
+  function Title({ title }) {
+    if (title.length > 33) {
+      let str = "";
+      for (let i = 0; i < 33; i++) {
+        str += title[i];
+      }
+      str += "...";
+      return str;
+    } else {
+      return title;
+    }
+  }
+
+  /* Truncate long author names */
+  function Author({ author }) {
+    if (author.length > 20) {
+      let str = "";
+      for (let i = 0; i < 19; i++) {
+        str += author[i];
+      }
+      str += "...";
+      return str;
+    } else {
+      return author;
+    }
+  }
+
+  /* Display error message if no cover is found */
+  function BookCover({ coverId }) {
+    if (coverId) {
+      return (
+        <Image
+          src={`https://covers.openlibrary.org/b/id/${props.card.cover_id}-L.jpg`}
+          alt=""
+          borderRadius="lg" // sets image clarity (S=blurry, L=HD)
+          boxSize="400px" // sets image height
+        />
+      );
+    } else {
+      return (
+        <Box
+          w="250px"
+          h="400px"
+          align-items="center"
+          bg="gray.100"
+          border="solid"
+          borderRadius="lg"
+          borderColor="gray.200"
+          borderWidth="1"
+        >
+          <Text fontSize="4xl" mt="120px" textColor="gray.300">
+            Cover not available
+          </Text>
+        </Box>
+      );
+    }
+  }
+
+  /* Formula to generate price from book title */
+  function GetBookPrice({ title }) {
+    let sumOfAscii = 0;
+    for (var i = 0; i < title.length; i++) {
+      sumOfAscii += title.charCodeAt(i);
+    }
+    /* If title is undefined or non-English */
+    if (sumOfAscii == 0) {
+      sumOfAscii = 1000;
     }
 
-    /* Truncate long author names */
-    function Author({ author }) {
-        if (author.length > 20) {
-            let str = '';
-            for (let i = 0; i < 19; i++) {
-                str += author[i];
-            }
-            str += '...';
-            return str;
-        } else {
-            return author;
-        }
+    /* We want prices to be $10-25 */
+    while (sumOfAscii < 1000) {
+      sumOfAscii += 1000;
     }
+    while (sumOfAscii > 2500) {
+      sumOfAscii -= 1000;
+    }
+    return (sumOfAscii / 100).toFixed(2); // keep trailing zeros
+  }
 
-    /* Display error message if no cover is found */
-    function BookCover({ coverId }) {
-        if (coverId) {
-            return (
-                <Image
-                    src={`https://covers.openlibrary.org/b/id/${props.card.cover_id}-L.jpg`}
-                    alt=""
-                    borderRadius="lg" // sets image clarity (S=blurry, L=HD)
-                    boxSize="400px" // sets image height
-                />
-            );
-        } else {
-            return (
-                <Box
-                    w="250px"
-                    h="400px"
-                    align-items="center"
-                    bg="gray.100"
-                    border="solid"
-                    borderRadius="lg"
-                    borderColor="gray.200"
-                    borderWidth="1"
-                >
-                    <Text fontSize="4xl" mt="120px" textColor="gray.300">
-                        Cover not available
+  return (
+    <Flex justify-content="space-evenly">
+      {/* Need align center in Box for image */}
+      <Box w="300px" align="center">
+        <Card
+          maxW="sm"
+          boxShadow="xl"
+          my={{ base: 5, md: 50 }}
+          fontFamily="Poppins"
+          fontSize="sm"
+        >
+          <CardBody>
+            <BookCover coverId={props.card.cover_id} />
+            {/* Set box width for author and price */}
+            <Box w="250px">
+              <Stack mt="6" spacing="3">
+                <Heading size="xs" align="left">
+                  {/* Shorten titles to fit on card */}
+                  <Title title={props.card.title} />
+                </Heading>
+                <Text noOfLines={1} align="left">
+                  {props.card.subject[0]}
+                </Text>
+              </Stack>
+              <Divider mt="3" mb="5" borderColor="#D9D9D9" />
+              <Grid templateRows="repeat(2, 1fr)" gap={2} mb="5">
+                <GridItem w="100%" h="5">
+                  <HStack>
+                    {/* Author should take up ~75% of box width */}
+                    <Box width="195px" align="left">
+                      <Text color="#61625F">Author</Text>
+                    </Box>
+                    <Text color="#61625F">Price</Text>
+                  </HStack>
+                </GridItem>
+                <GridItem w="100%" h="5">
+                  <HStack>
+                    {/* Author should take up ~75% of box width */}
+                    <Box width="195px" align="left">
+                      <Text as="b" fontSize="sm">
+                        {/* Shorten author names to fit on card */}
+                        <Author
+                          author={
+                            props.card.authors[0]
+                              ? props.card.authors[0].name
+                              : "No author data available"
+                          }
+                        />
+                      </Text>
+                    </Box>
+                    <Text as="b">
+                      $
+                      <GetBookPrice title={props.card.title} />
                     </Text>
-                </Box>
-            );
-        }
-    }
-
-    /* Formula to generate price from book title */
-    function GetBookPrice({ title }) {
-        let sumOfAscii = 0;
-        for (var i = 0; i < title.length; i++) {
-            sumOfAscii += title.charCodeAt(i);
-        }
-        /* If title is undefined or non-English */
-        if (sumOfAscii == 0) {
-            sumOfAscii = 1000;
-        }
-
-        /* We want prices to be $10-25 */
-        while (sumOfAscii < 1000) {
-            sumOfAscii += 1000;
-        }
-        while (sumOfAscii > 2500) {
-            sumOfAscii -= 1000;
-        }
-        return (sumOfAscii / 100).toFixed(2); // keep trailing zeros
-    }
-
-    return (
-        <Flex justify-content="space-evenly">
-            {/* Need align center in Box for image */}
-            <Box w="300px" align="center">
-                <Card
-                    maxW="sm"
-                    boxShadow="xl"
-                    my={{ base: 5, md: 50 }}
-                    fontFamily="Poppins"
-                    fontSize="sm"
+                  </HStack>
+                </GridItem>
+              </Grid>
+              <Stack align="center">
+                <Button
+                  bgColor="white"
+                  color="#E4573D"
+                  colorScheme="E4573D"
+                  size="sm"
+                  rounded="sm"
+                  px="12"
+                  py="6"
+                  mt="2"
+                  variant="outline"
+                  _hover={{ bg: "#E4573D", color: "white" }}
+                  letterSpacing="2px"
+                  onClick={(e) => {
+                    const justKey = props.bookKey.replace("/works", "");
+                    navigate("/book" + justKey);
+                  }}
                 >
-                    <CardBody>
-                        <BookCover coverId={props.card.cover_id} />
-                        {/* Set box width for author and price */}
-                        <Box w="250px">
-                            <Stack mt="6" spacing="3">
-                                <Heading size="xs" align="left">
-                                    {/* Shorten titles to fit on card */}
-                                    <Title title={props.card.title} />
-                                </Heading>
-                            </Stack>
-                            <Divider mt="3" mb="5" borderColor="#D9D9D9" />
-                            <Grid templateRows="repeat(2, 1fr)" gap={2} mb="5">
-                                <GridItem w="100%" h="5">
-                                    <HStack>
-                                        {/* Author should take up ~75% of box width */}
-                                        <Box width="195px" align="left">
-                                            <Text color="#61625F">Author</Text>
-                                        </Box>
-                                        <Text color="#61625F">Price</Text>
-                                    </HStack>
-                                </GridItem>
-                                <GridItem w="100%" h="5">
-                                    <HStack>
-                                        {/* Author should take up ~75% of box width */}
-                                        <Box width="195px" align="left">
-                                            <Text as="b" fontSize="xs">
-                                                {/* Shorten author names to fit on card */}
-                                                <Author
-                                                    author={
-                                                        props.card.authors[0]
-                                                            ? props.card
-                                                                  .authors[0]
-                                                                  .name
-                                                            : 'No author data available'
-                                                    }
-                                                />
-                                            </Text>
-                                        </Box>
-                                        <Text as="b">
-                                            $
-                                            <GetBookPrice
-                                                title={props.card.title}
-                                            />
-                                        </Text>
-                                    </HStack>
-                                </GridItem>
-                            </Grid>
-                            <Stack align="center">
-                                <Button
-                                    bgColor="white"
-                                    color="#E4573D"
-                                    colorScheme="E4573D"
-                                    size="sm"
-                                    rounded="sm"
-                                    px="12"
-                                    py="6"
-                                    mt="2"
-                                    variant="outline"
-                                    _hover={{ bg: '#E4573D', color: 'white' }}
-                                    letterSpacing="2px"
-                                    onClick={(e) => {
-                                        const justKey = props.bookKey.replace(
-                                            '/works',
-                                            ''
-                                        );
-                                        navigate('/book' + justKey);
-                                    }}
-                                >
-                                    More Details
-                                </Button>
-                            </Stack>
-                        </Box>
-                    </CardBody>
-                </Card>
+                  More Details
+                </Button>
+              </Stack>
             </Box>
-        </Flex>
-    );
+          </CardBody>
+        </Card>
+      </Box>
+    </Flex>
+  );
 }
--- book-store/src/components/Categories/CategoriesFilterFunc.js
+++ book-store/src/components/Categories/CategoriesFilterFunc.js
@@ -1,70 +1,68 @@
-import { GridItem } from '@chakra-ui/react';
-import { useParams, useNavigate } from 'react-router-dom';
+import { GridItem } from "@chakra-ui/react";
+import { useParams, useNavigate } from "react-router-dom";
 
 // Must manually specify extension; too many variations
 // 15 categories per (non)fiction
 const FICTION_SUBJECTS = [
-    ['Romance', 'fiction_romance_general'],
-    ['Thrillers', 'fiction_thrillers_general'],
-    ['Mystery & detective', 'fiction_mystery_&_detective_general'],
-    ['Horror', 'fiction_horror'],
-    ["Children's fiction", "children's_fiction"],
-    ['Comics & graphic novels', 'comics_&_graphic_novels_general'],
-    ['Fantasy', 'fiction_fantasy_general'],
-    ['Humor', 'humor'],
-    ['Action & adventure', 'fiction_action_&_adventure'],
-    ['Tragedy', 'tragedy'],
-    ['Young adult', 'young_adult_fiction'],
-    ['Juvenile', 'juvenile_fiction'],
-    ['Folklore', 'folklore'],
-    ['Short stories', 'short_stories'],
-    ['Science fiction', 'fiction_science_fiction_general']
+  ["Romance", "fiction_romance_general"],
+  ["Thrillers", "fiction_thrillers_general"],
+  ["Mystery & detective", "fiction_mystery_&_detective_general"],
+  ["Horror", "fiction_horror"],
+  ["Children's fiction", "children's_fiction"],
+  ["Comics & graphic novels", "comics_&_graphic_novels_general"],
+  ["Fantasy", "fiction_fantasy_general"],
+  ["Humor", "humor"],
+  ["Action & adventure", "fiction_action_&_adventure"],
+  ["Tragedy", "tragedy"],
+  ["Young adult", "young_adult_fiction"],
+  ["Juvenile", "juvenile_fiction"],
+  ["Folklore", "folklore"],
+  ["Short stories", "short_stories"],
+  ["Science fiction", "fiction_science_fiction_general"],
 ].sort();
 
 const NONFICTION_SUBJECTS = [
-    ['Finance', 'finance'],
-    ['Biography', 'biography'],
-    ['Travel', 'travel'],
-    ['Business', 'business'],
-    ['Religion', 'religion'],
-    ['Cookbooks', 'cookbooks'],
-    ['Self-help', 'self-help'],
-    ['Psychology', 'psychology'],
-    ['Music', 'music'],
-    ['Education', 'education'],
-    ['Anthropology', 'anthropology'],
-    ['Environment', 'environment'],
-    ['Sports', 'sports'],
-    ['Science', 'science'],
-    ['Politics & government', 'politics_and_government']
+  ["Finance", "finance"],
+  ["Biography", "biography"],
+  ["Travel", "travel"],
+  ["Business", "business"],
+  ["Religion", "religion"],
+  ["Cookbooks", "cookbooks"],
+  ["Self-help", "self-help"],
+  ["Psychology", "psychology"],
+  ["Music", "music"],
+  ["Education", "education"],
+  ["Anthropology", "anthropology"],
+  ["Environment", "environment"],
+  ["Sports", "sports"],
+  ["Science", "science"],
+  ["Politics & government", "politics_and_government"],
 ].sort();
 
 export default function CategoriesFilter({ fiction }) {
-    const { param } = useParams();
-    const navigate = useNavigate();
-    const handleSelectCategory = (id) => {
-        navigate(`/categories/${id}`);
-    };
+  const { param } = useParams();
+  const navigate = useNavigate();
+  const handleSelectCategory = (id) => {
+    navigate(`/categories/${id}`);
+  };
 
-    return (
-        <>
-            {(fiction ? FICTION_SUBJECTS : NONFICTION_SUBJECTS).map(
-                (item, index) => {
-                    return (
-                        <GridItem>
-                            <a
-                                onClick={() =>
-                                    // Don't directly use navigate() or cannot go back to Categories after going to BookPage
-                                    handleSelectCategory(`${item[1]}`)
-                                }
-                                id={item[0]}
-                            >
-                                {item[0]}
-                            </a>
-                        </GridItem>
-                    );
-                }
-            )}
-        </>
-    );
+  return (
+    <>
+      {(fiction ? FICTION_SUBJECTS : NONFICTION_SUBJECTS).map((item, index) => {
+        return (
+          <GridItem _hover={{ color: "#E4573D" }}>
+            <a
+              onClick={() =>
+                // Don't directly use navigate() or cannot go back to Categories after going to BookPage
+                handleSelectCategory(`${item[1]}`)
+              }
+              id={item[0]}
+            >
+              {item[0]}
+            </a>
+          </GridItem>
+        );
+      })}
+    </>
+  );
 }
